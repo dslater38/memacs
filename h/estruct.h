8e8217d0a3fa80af54e488234a231b0169cba18a
Daniel R. Slater <daniel.slater@thermofisher.com>
2022-09-03 12:26:18 -0400

    Fixed  merge error.

--- h/estruct.h
+++ h/estruct.h
@@ -1,21 +1,19 @@
 #ifndef ESTRUCT_H_INCLUDED
 #define ESTRUCT_H_INCLUDED
 
-#ifdef BUILD_MSWIN
-#include "mswin/estruct.h"
+#if defined(BUILD_MSWIN)
+#   include "mswin/estruct.h"
 #elif defined(BUILD_NTCONSOLE)
-#include "nt/estruct.h"
+#   include "nt/estruct.h"
 #elif defined(BUILD_XPCONSOLE)
-#include "xp/estruct.h"
+#   include "xp/estruct.h"
 #elif defined(BUILD_LINUX)
 #	include "linux/estruct.h"
+#elif defined(BUILD_LINUX)
+#   include "linux/estruct.h"
 #else
-#ifdef BUILD_LINUX
-#include "linux/estruct.h"
-#else
-#include "estruct_orig.h"
-#endif
-#endif
+#   include "estruct_orig.h"
 #endif
 
-#endif            
+
+#endif
